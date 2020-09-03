d01ed6a3c99bd1813398e9fd27f80bb8f2c65394
kristapsrit <kiyuuman@gmail.com>
2020-09-03 14:55:55 +0300

    changes

--- single.php
+++ single.php
@@ -4,7 +4,25 @@
     <div class="page-content">
         <?php while (have_posts()) : the_post(); ?>
             <div class="post-content">
-                <h1><?php the_title(); ?></h1>
+                <div class="header-elements">
+                    <div class="header-elements-left">
+                        <img src="<?php echo BETSPIN_DIR_URI . "/dist/img/header-cards.png"; ?>" alt="Cards">
+                        <h2><?php echo the_title(); ?></h2>
+                    </div>
+                    <div class="header-elements-right">
+                        <div class="single-post-date">
+                            <p>Published by Betspin<span><?php the_date("d.m.y"); ?></span></p>
+                        </div>
+                        <div class="post-share">
+                            <a href="http://www.facebook.com/sharer/sharer.php?u=<?php echo urlencode(get_permalink()); ?>">
+                                <img src="<?php echo BETSPIN_DIR_URI . "/dist/img/facebook.png"; ?>" alt="">
+                            </a>
+                            <a class="twitter-share-button" href="https://twitter.com/intent/tweet">
+                                <img src="<?php echo BETSPIN_DIR_URI . "/dist/img/twitter.png"; ?>" alt="Twitter">
+                            </a>
+                        </div>
+                    </div>
+                </div>
                 <?php the_content(); ?>
             </div>
         <?php endwhile; ?>
