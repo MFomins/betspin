0baab960f83930fd940c612d7ff2535bc8d8c143
kristapsrit <kiyuuman@gmail.com>
2020-09-09 17:05:53 +0300

    blocks added

--- inc/acf-blocks/testimonial.php
+++ inc/acf-blocks/testimonial.php
@@ -1,1 +1,15 @@
-<?php echo 'asd'; ?>
+<div class="g-block-wrap">
+    <div class="g-block-img">
+        <?php
+        $image = get_field('betspin_block_image');
+
+        echo "<img src='{$image['url']}' alt='{$image['alt']}'>"
+        ?>
+    </div>
+    <div class="g-block-header">
+        <h4><?php the_field('betspin_block_header'); ?></h4>
+    </div>
+    <div class="g-block-text">
+        <p><?php the_field('betspin_block_text'); ?></p>
+    </div>
+</div>
--- inc/acf-fields.php
+++ inc/acf-fields.php
@@ -24,6 +24,92 @@ function my_acf_init_block_types()
 if (function_exists('acf_add_local_field_group')) :
 
     acf_add_local_field_group(array(
+        'key' => 'group_5f58da350e0e0',
+        'title' => 'Block',
+        'fields' => array(
+            array(
+                'key' => 'field_5f58da3d9620a',
+                'label' => 'Image',
+                'name' => 'betspin_block_image',
+                'type' => 'image',
+                'instructions' => '',
+                'required' => 0,
+                'conditional_logic' => 0,
+                'wrapper' => array(
+                    'width' => '',
+                    'class' => '',
+                    'id' => '',
+                ),
+                'return_format' => 'array',
+                'preview_size' => 'thumbnail',
+                'library' => 'all',
+                'min_width' => '',
+                'min_height' => '',
+                'min_size' => '',
+                'max_width' => '',
+                'max_height' => '',
+                'max_size' => '',
+                'mime_types' => '',
+            ),
+            array(
+                'key' => 'field_5f58db0e36d25',
+                'label' => 'Header',
+                'name' => 'betspin_block_header',
+                'type' => 'text',
+                'instructions' => '',
+                'required' => 0,
+                'conditional_logic' => 0,
+                'wrapper' => array(
+                    'width' => '',
+                    'class' => '',
+                    'id' => '',
+                ),
+                'default_value' => '',
+                'placeholder' => '',
+                'prepend' => '',
+                'append' => '',
+                'maxlength' => '',
+            ),
+            array(
+                'key' => 'field_5f58da5b9620b',
+                'label' => 'Text',
+                'name' => 'betspin_block_text',
+                'type' => 'text',
+                'instructions' => '',
+                'required' => 0,
+                'conditional_logic' => 0,
+                'wrapper' => array(
+                    'width' => '',
+                    'class' => '',
+                    'id' => '',
+                ),
+                'default_value' => '',
+                'placeholder' => '',
+                'prepend' => '',
+                'append' => '',
+                'maxlength' => '',
+            ),
+        ),
+        'location' => array(
+            array(
+                array(
+                    'param' => 'block',
+                    'operator' => '==',
+                    'value' => 'acf/casino-games',
+                ),
+            ),
+        ),
+        'menu_order' => 0,
+        'position' => 'normal',
+        'style' => 'default',
+        'label_placement' => 'top',
+        'instruction_placement' => 'label',
+        'hide_on_screen' => '',
+        'active' => true,
+        'description' => '',
+    ));
+
+    acf_add_local_field_group(array(
         'key' => 'group_5efeeea29ab6b',
         'title' => 'Footer Options',
         'fields' => array(
