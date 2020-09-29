ea5a445e5840295621910e1ab8e0170ef9cddafb
kristapsrit <kiyuuman@gmail.com>
2020-09-29 21:48:03 +0300

    style

--- inc/acf-fields.php
+++ inc/acf-fields.php
@@ -257,6 +257,73 @@ if (function_exists('acf_add_local_field_group')) :
     ));
 
     acf_add_local_field_group(array(
+        'key' => 'group_5f6dbc4c15449',
+        'title' => 'Gutenberg Games Block',
+        'fields' => array(
+            array(
+                'key' => 'field_5f6dbc4c21acb',
+                'label' => 'Image',
+                'name' => 'betspin_games_image',
+                'type' => 'image',
+                'instructions' => '',
+                'required' => 0,
+                'conditional_logic' => 0,
+                'wrapper' => array(
+                    'width' => '',
+                    'class' => '',
+                    'id' => '',
+                ),
+                'return_format' => 'array',
+                'preview_size' => 'thumbnail',
+                'library' => 'all',
+                'min_width' => '',
+                'min_height' => '',
+                'min_size' => '',
+                'max_width' => '',
+                'max_height' => '',
+                'max_size' => '',
+                'mime_types' => '',
+            ),
+            array(
+                'key' => 'field_5f6dbc4c21ea1',
+                'label' => 'Header',
+                'name' => 'betspin_games_header',
+                'type' => 'text',
+                'instructions' => '',
+                'required' => 0,
+                'conditional_logic' => 0,
+                'wrapper' => array(
+                    'width' => '',
+                    'class' => '',
+                    'id' => '',
+                ),
+                'default_value' => '',
+                'placeholder' => '',
+                'prepend' => '',
+                'append' => '',
+                'maxlength' => '',
+            ),
+        ),
+        'location' => array(
+            array(
+                array(
+                    'param' => 'block',
+                    'operator' => '==',
+                    'value' => 'acf/games-block',
+                ),
+            ),
+        ),
+        'menu_order' => 0,
+        'position' => 'normal',
+        'style' => 'default',
+        'label_placement' => 'top',
+        'instruction_placement' => 'label',
+        'hide_on_screen' => '',
+        'active' => true,
+        'description' => '',
+    ));
+
+    acf_add_local_field_group(array(
         'key' => 'group_5eec9447c96e9',
         'title' => 'Menu Icons',
         'fields' => array(
