d0fdbc37cfd91550f915d1a798968054a74571c6
kristapsrit <kiyuuman@gmail.com>
2020-10-07 12:08:25 +0300

    ver

--- inc/acf-fields.php
+++ inc/acf-fields.php
@@ -392,6 +392,73 @@ if (function_exists('acf_add_local_field_group')) :
     ));
 
     acf_add_local_field_group(array(
+        'key' => 'group_5f7d7d6605ae4',
+        'title' => 'Theme Gutenberg Header Block',
+        'fields' => array(
+            array(
+                'key' => 'field_5f7d7d7a6f485',
+                'label' => 'Image',
+                'name' => 'betspin_header_image',
+                'type' => 'image',
+                'instructions' => '',
+                'required' => 0,
+                'conditional_logic' => 0,
+                'wrapper' => array(
+                    'width' => '',
+                    'class' => '',
+                    'id' => '',
+                ),
+                'return_format' => 'array',
+                'preview_size' => 'thumbnail',
+                'library' => 'all',
+                'min_width' => '',
+                'min_height' => '',
+                'min_size' => '',
+                'max_width' => '',
+                'max_height' => '',
+                'max_size' => '',
+                'mime_types' => '',
+            ),
+            array(
+                'key' => 'field_5f7d7e996f486',
+                'label' => 'Text',
+                'name' => 'betspin_header_text',
+                'type' => 'text',
+                'instructions' => '',
+                'required' => 0,
+                'conditional_logic' => 0,
+                'wrapper' => array(
+                    'width' => '',
+                    'class' => '',
+                    'id' => '',
+                ),
+                'default_value' => '',
+                'placeholder' => '',
+                'prepend' => '',
+                'append' => '',
+                'maxlength' => '',
+            ),
+        ),
+        'location' => array(
+            array(
+                array(
+                    'param' => 'block',
+                    'operator' => '==',
+                    'value' => 'acf/header-block',
+                ),
+            ),
+        ),
+        'menu_order' => 0,
+        'position' => 'normal',
+        'style' => 'default',
+        'label_placement' => 'top',
+        'instruction_placement' => 'label',
+        'hide_on_screen' => '',
+        'active' => true,
+        'description' => '',
+    ));
+
+    acf_add_local_field_group(array(
         'key' => 'group_5eec9447c96e9',
         'title' => 'Theme Menu Icons',
         'fields' => array(
