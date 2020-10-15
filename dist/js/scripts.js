f1e630419376f4c9ece31ce2249b56b130390792
kristapsrit <kiyuuman@gmail.com>
2020-10-15 19:19:15 +0300

    styles

--- dist/js/scripts.js
+++ dist/js/scripts.js
@@ -3,11 +3,17 @@ jQuery(document).ready(function ($) {
     $('.search-icon').on('click', function () {
         $('.search-overlay').css('display', 'flex');
         $('body').css('overflow-y', 'hidden');
+        $('.nav-search .icon-search').hide();
     });
 
     $('.overlay-close').on('click', function () {
         $('.search-overlay').css('display', 'none');
         $('body').css('overflow-y', 'initial');
+        $('.nav-search .icon-search').show();
+    });
+
+    $('#nav-icon').on('click', function(){
+        $('.slideout-menu').toggle();
     });
 
 });
