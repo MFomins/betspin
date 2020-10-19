7582dbbc76dc2f6f4f8b9445d91dad5ad8ab57b0
kristapsrit <kiyuuman@gmail.com>
2020-10-19 11:06:58 +0300

    404-page

--- 404.php
+++ 404.php
@@ -8,7 +8,23 @@
 get_header(); ?>
 
 <div class="template-404">
-    NOT FOUND TEMPLATE
+    <div class="container">
+        <div class="info-404">
+            <div class="info-404-wrap">
+                <h1>404 - All bets are off!</h1>
+                <p>It looks like the page you are looking for doesn't exist.</p>
+                <p>Please choose one of the following options:</p>
+                <ul>
+                    <li>- Visit the <b><a href="https://www.betspin.com">Betspin homepage</a></b> to navigate around the site</li>
+                    <li>- Use the <b>search function</b> above to find what you need</li>
+                    <li>- Go straight to one of the available <b>tables below</b></li>
+                </ul>
+                <div class="resp-container">
+                    <iframe class="resp-iframe" allowtransparency="true" src="https://keyaff.com/hl/?aid=669&amp;sid=1&amp;pbg=ffffff&amp;forcelogo=true&amp;rp=cto" scrolling="no" style="width: 890px;margin: 0 auto;display:block;" width="890" height="320" frameborder="0"></iframe>
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
 
 <?php get_footer(); ?>
