eefb29cf760a70ff391865d4f0641fa9b404e8dc
kristapsrit <kiyuuman@gmail.com>
2020-10-19 11:30:43 +0300

    styles

--- 404.php
+++ 404.php
@@ -19,8 +19,8 @@ get_header(); ?>
                     <li>- Use the <b>search function</b> above to find what you need</li>
                     <li>- Go straight to one of the available <b>tables below</b></li>
                 </ul>
-                <div class="resp-container">
-                    <iframe class="resp-iframe" allowtransparency="true" src="https://keyaff.com/hl/?aid=669&amp;sid=1&amp;pbg=ffffff&amp;forcelogo=true&amp;rp=cto" scrolling="no" style="width: 890px;margin: 0 auto;display:block;" width="890" height="320" frameborder="0"></iframe>
+                <div class="iframe-404-container">
+                    <iframe class="responsive-iframe" allowtransparency="true" src="https://keyaff.com/hl/?aid=669&amp;sid=1&amp;pbg=ffffff&amp;forcelogo=true&amp;rp=cto" scrolling="no" style="width: 890px;margin: 0 auto;display:block;" width="890" height="320" frameborder="0"></iframe>
                 </div>
             </div>
         </div>
