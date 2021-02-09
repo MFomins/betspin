45862e049dce54b5b3071e5cebaba61674cc2824
Kristaps Ritins <kristaps@yoursite.lv>
2021-02-09 13:25:35 +0200

    tac link added into terms component

--- assets/js/widget-toplist.js
+++ assets/js/widget-toplist.js
@@ -53,7 +53,7 @@ const MyCustomTerms = data => {
     let tncChecked = data.extraFields.terms_and_conditions_link_enabled;
     let tnc = data.extraFields.terms_and_condition_link_text;
     return <div className="terms_conditions">
-        <p>{isChecked === "1" ? terms : ''}<a href={data.operatorLink}>{tncChecked === "1" ? tnc : ''}</a></p>
+        <p>{isChecked === "1" ? terms : ''}<a href={data.tacLink}>{tncChecked === "1" ? tnc : ''}</a></p>
     </div>;
 };
 
